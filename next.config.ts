df48e1b2c3c8d73817a0bc4322024265ad908191
Sam Daramroei <31776791+cheerios-design@users.noreply.github.com>
2026-02-14 02:16:53 +0300

    to publish in Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Your Next.js config
+  output: 'export',
+  basePath: '/valentines',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
